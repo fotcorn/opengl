6201f25e8741a1f67fc74fffbad3e9781cf99ee4
fotcorn <fotcorn@gmail.com>
2018-11-06 20:55:51 +0100

    heightmap: make heightmap white

--- shaders/heightmap_fragment.glsl
+++ shaders/heightmap_fragment.glsl
@@ -4,5 +4,6 @@ in vec3 frag_position;
 out vec4 fragmentColor;
 
 void main() {
-  fragmentColor = vec4(0.5 + frag_position.y / 32, 0.0, 0.0, 1.0);
+  float color = 0.3 + frag_position.y / 32;
+  fragmentColor = vec4(color, color, color, 1.0);
 }
